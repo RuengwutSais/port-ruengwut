1e2a6f5571087ffa9ddd0c7d1271bb38b5154397
Ruengwut Saisawat <Ruengwut.sais@gmail.com>
2023-11-27 20:00:57 +0700

    add workspace

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -3,8 +3,8 @@ import FooterComponent from "./footer";
 import "../css/projectStyle.css";
 import rentCentral from "../logo/1.png";
 import miWeather from "../logo/2.png";
-import backgroundStyles from '../css/background';
-
+import filmpass from "../logo/3.png";
+import backgroundStyles from "../css/background";
 
 const ProjectComponent = () => {
   return (
@@ -35,30 +35,58 @@ const ProjectComponent = () => {
         </div>
         <div className="workspace-section">
           <div className="card-workspace">
-            <div className="workspace-div">
-              <h1>Workspace</h1>
-              <div className="miweater-div">
-                <a
-                  href="https://miweather-workspace.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={miWeather} alt="workspace-1" />
-                  <div className="miweater-overlay">
-                    <h1>Click to view!</h1>
-                  </div>
-                </a>
+            <div>
+              <div className="workspace-div">
+                <h1>Workspace</h1>
+                <div className="miweater-div">
+                  <a
+                    href="https://miweather-workspace.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={miWeather} alt="workspace-1" />
+                    <div className="miweater-overlay">
+                      <h1>Click to view!</h1>
+                    </div>
+                  </a>
+                </div>
               </div>
+              <h2>MiWeather</h2>
+              <h5>2023</h5>
+              <h4>Overview:</h4>
+              <p>
+                An practice workspace of a weather dashboard by calling an API
+                from OpenWeather.com
+              </p>
+              <h4>Tools:</h4>
+              <p>HTML, CSS, JavaScript, vue.js, Bootstrap, FontAwesome</p>
             </div>
-            <h2>MiWeather</h2>
-            <h5>2023</h5>
-            <h4>Overview:</h4>
-            <p>
-              An practice workspace of a weather dashboard by calling an API
-              from OpenWeather.com
-            </p>
-            <h4>Tools:</h4>
-            <p>HTML, CSS, JavaScript, vue.js, Bootstrap, FontAwesome</p>
+            <br />
+            <div className="break-work">
+              <div className="workspace-div">
+                <div className="miweater-div">
+                  <a
+                    href="https://filmic-pass.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={filmpass} alt="workspace-1" />
+                    <div className="miweater-overlay">
+                      <h1>Click to view!</h1>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <h2>FilmicPass</h2>
+              <h5>2023</h5>
+              <h4>Overview:</h4>
+              <p>
+                Create a movie search app that allows users to search for
+                information about movies using an external API
+              </p>
+              <h4>Tools:</h4>
+              <p>HTML, CSS, JavaScript, React.js, Tailwind, FontAwesome</p>
+            </div>
           </div>
         </div>
       </div>
